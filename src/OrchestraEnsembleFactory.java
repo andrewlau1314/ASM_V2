16e906d12c4bcfcfdc3a22d9655a45e89302a02f
andrewlau1314 <laujamly@gmail.com>
2025-11-12 23:51:04 +0800

    First Commit

--- src/OrchestraEnsembleFactory.java
+++ src/OrchestraEnsembleFactory.java
@@ -1,7 +1,7 @@
 public class OrchestraEnsembleFactory implements EnssembleFactroy {
     public Ensemble CreateEnsemble(String eID , String eName){
 
-        OrchestraEnsemble Orchestra = new OrchestraEnsemble(String eID ,String eName);
+        OrchestraEnsemble Orchestra = new OrchestraEnsemble(eID);
         System.out.println("Orchestra Ensemble Created with ID: " + eID);
         return Orchestra;
     }
